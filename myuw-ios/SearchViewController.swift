fa8cdbf86577805d9ceb02539c67ea28103b3b4c
Charlon Palacay <charlon@gmail.com>
2019-12-13 16:17:04 -0800

    Initial search controller interaction.

--- myuw-ios/SearchViewController.swift
+++ myuw-ios/SearchViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class SearchViewController: CustomWebViewController {
+class SearchViewController: CustomWebViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +24,44 @@ class SearchViewController: CustomWebViewController {
         //self.navigationItem.largeTitleDisplayMode = .never
         
         // search controler and bar setup
-        let mySearchController = UISearchController()
-        self.navigationItem.searchController = mySearchController
+        let searchController = UISearchController()
+        self.navigationItem.searchController = searchController
         self.navigationItem.hidesSearchBarWhenScrolling = false
-        mySearchController.searchBar.placeholder = "Search"
-        mySearchController.searchBar.tintColor = .white
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.tintColor = .white
+        
+        searchController.searchBar.delegate = self
         
     }
+    
+    func searchBarSearchButtonClicked( _ searchBar: UISearchBar)
+    {
+        print("serach bar clicked: ", searchBar.text! )
+        let visitUrl:String = "https://www.washington.edu/search/?q=\(searchBar.text ?? "")"
+        
+        webView.load(visitUrl)
 
+    }
+    
+    override func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        // on webview finish... set scroll behavior back to automatic
+        webView.scrollView.contentInsetAdjustmentBehavior = .automatic
+        activityIndicator.isHidden = true
+        activityIndicator.stopAnimating()
+        
+        // override navigation title by getting the navigated webview's page title
+        self.navigationItem.title = webView.title!.replacingOccurrences(of: "MyUW: ", with: "")
+        
+        // dynamically inject css file into webview
+
+        guard let path = Bundle.main.path(forResource: "search", ofType: "css") else { return }
+        let css = try! String(contentsOfFile: path).replacingOccurrences(of: "\\n", with: "", options: .regularExpression)
+        let js = "var style = document.createElement('style'); style.innerHTML = '\(css)'; document.head.appendChild(style);"
+        webView.evaluateJavaScript(js)
+    
+        
+    }
+    
 }
+
