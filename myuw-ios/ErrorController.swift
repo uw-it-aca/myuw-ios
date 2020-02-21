5a99f6e089c6fee7e4609069262b968a5b363cf1
Charlon Palacay <charlon@gmail.com>
2020-02-21 08:29:52 -0800

    Change error messages based on internet connection.

--- myuw-ios/ErrorController.swift
+++ myuw-ios/ErrorController.swift
@@ -15,6 +15,7 @@ class ErrorController: UIViewController {
     let headerText = UILabel()
     let bodyText = UILabel()
     let signInButton = UIButton()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,7 @@ class ErrorController: UIViewController {
         headerText.layer.borderColor = UIColor.red.cgColor
         headerText.font = UIFont.boldSystemFont(ofSize: 18)
         headerText.textAlignment = .left
-        headerText.text = "Unable to load page"
+        //headerText.text = "Unable to load page"
         headerText.sizeToFit()
         view.addSubview(headerText)
         // autolayout contraints
@@ -49,7 +50,7 @@ class ErrorController: UIViewController {
         bodyText.textAlignment = .left
         bodyText.numberOfLines = 0
         bodyText.sizeToFit()
-        bodyText.text = "A server or network error has occurred. We are aware of the issue and are working on it. If you are no longer connected to the internet, please fix the issue and try again in a few minutes."
+        //bodyText.text = "A server or network error has occurred. We are aware of the issue and are working on it. If you are no longer connected to the internet, please fix the issue and try again in a few minutes."
         view.addSubview(bodyText)
         // autolayout contraints
         bodyText.translatesAutoresizingMaskIntoConstraints = false
@@ -72,6 +73,14 @@ class ErrorController: UIViewController {
         // set topanchor of label equal to bottomanchor of textview
         signInButton.topAnchor.constraint(equalTo: bodyText.bottomAnchor, constant: 10).isActive = true
         
+        if (appDelegate.isConnectedToNetwork()) {
+            headerText.text = "Unable to load page"
+            bodyText.text = "A server or network error has occurred. We are aware of the issue and are working on it. "
+        } else {
+            headerText.text = "No internet connection"
+            bodyText.text = "If you are no longer connected to the internet, please fix the issue and try again in a few minutes."
+        }
+        
     }
     
     @objc private func retryNetwork() {
@@ -80,7 +89,7 @@ class ErrorController: UIViewController {
         // force use go through appAuth flow when foregrounding the app
         let appAuthController = AppAuthController()
         let navController = UINavigationController(rootViewController: appAuthController)
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
         // set appAuth controller as rootViewController
         appDelegate.window!.rootViewController = navController
     }
