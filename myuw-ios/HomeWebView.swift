052520a7d470b864354fd30f436018571a21eddd
Craig M. Stimmel <cstimmel@uw.edu>
2026-01-02 16:29:57 -0800

    Adjust top buttons so text isn't cut off

--- myuw-ios/HomeWebView.swift
+++ myuw-ios/HomeWebView.swift
@@ -20,37 +20,47 @@ class HomeWebView: WebViewController {
         // override navigation title
         self.navigationItem.title = "MyUW"
         
-        // define custom user button
         let userButton = UIButton(type: .system)
         userButton.setImage(UIImage(named: "ic_person"), for: .normal)
-        userButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0);
-        userButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -5);
+        userButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        userButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5)
         userButton.setTitle(User.userNetID, for: .normal)
         userButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
         userButton.sizeToFit()
         userButton.addTarget(self, action: #selector(showProfile), for: .touchUpInside)
+
+        // Set a minimum width
+        let minWidth: CGFloat = 100
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: max(userButton.frame.width, minWidth), height: userButton.frame.height))
+        userButton.frame = containerView.bounds
+        containerView.addSubview(userButton)
        
         // show search funtionality for ios13 devices only
         if #available(iOS 13.0, *) {
             // define custom search button
             let searchButton = UIButton(type: .system)
             searchButton.setImage(UIImage(named: "ic_search_18"), for: .normal)
-            searchButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0);
-            searchButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: -8, bottom: 0, right: 0);
+            searchButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0);
+            searchButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5);
             searchButton.setTitle("Search", for: .normal)
             searchButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
             searchButton.sizeToFit()
             searchButton.addTarget(self, action: #selector(showSearch), for: .touchUpInside)
             
             // add search button in navbar programatically
-            let searchBarButtonItem = UIBarButtonItem(customView: searchButton)
+            let minSearchWidth: CGFloat = 80
+            let searchContainerView = UIView(frame: CGRect(x: 0, y: 0, width: max(searchButton.frame.width, minSearchWidth), height: searchButton.frame.height))
+            searchButton.frame = searchContainerView.bounds
+            searchContainerView.addSubview(searchButton)
+
+            let searchBarButtonItem = UIBarButtonItem(customView: searchContainerView)
             self.navigationItem.rightBarButtonItem = searchBarButtonItem
         }
         
         // add a user and search buttons in navbar programatically
-        let userBarButtonItem = UIBarButtonItem(customView: userButton)
+        //let userBarButtonItem = UIBarButtonItem(customView: userButton)
+        let userBarButtonItem = UIBarButtonItem(customView: containerView)
         self.navigationItem.leftBarButtonItem = userBarButtonItem
-    
     }
     
     @objc func showProfile() {
